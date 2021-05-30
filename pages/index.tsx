771f92f473adb70d7f9b4be9825635663eb9d0bc
hi019 <65871571+hi019@users.noreply.github.com>
2021-05-29 22:50:01 -0400

    Remove unneeded css and imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,3 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-
 export default function Home() {
     return (
         <div className='w-screen h-screen flex justify-center items-center text-2xl'>
